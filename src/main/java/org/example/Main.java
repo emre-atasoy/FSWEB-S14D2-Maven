2673f6611b1073060f8a24253d467cebe77a97eb
emreatasoy <yunusatasoyy16@gmail.com>
2025-09-16 17:48:21 +0300

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Wall wall1 = new Wall("North");
+        Wall wall2 = new Wall("South");
+        Wall wall3 = new Wall("East");
+        Wall wall4 = new Wall("West");
+
+        Ceiling ceiling = new Ceiling(250, PaintColor.WHITE);
+        Bed bed = new Bed("Modern", 2, 60, 2, 1);
+        Lamp lamp = new Lamp(LampType.NEON, true, 85);
+        Wardrobe wardrobe = new Wardrobe(120, 200, 75.5);
+        Carpet carpet = new Carpet(300, 400, PaintColor.RED);
+
+        Bedroom bedroom = new Bedroom("Master Bedroom", wall1, wall2, wall3, wall4,
+                ceiling, bed, lamp, wardrobe, carpet);
+
+        // Metod testleri
+        bedroom.getWall1().create();
+        bedroom.getCeiling().create();
+        bedroom.getBed().make();
+        bedroom.getLamp().turnOn();
+        bedroom.getWardrobe().add();
+        bedroom.getCarpet().lying();
     }
 }
