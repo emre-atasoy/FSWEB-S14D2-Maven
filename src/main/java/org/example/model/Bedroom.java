0f97844781e11525083ebdae036678c8e6e2b4c6
emreatasoy <yunusatasoyy16@gmail.com>
2025-09-16 23:44:32 +0300

    Challange done.

--- src/main/java/org/example/model/Bedroom.java
+++ src/main/java/org/example/model/Bedroom.java
@@ -1,54 +1,21 @@
 package org.example.model;
 
-public class Bedroom {
+public class Bedroom extends Room {
 
-    private String name;
-    private Wall wall1;
-    private Wall wall2;
-    private Wall wall3;
-    private Wall wall4;
-    private Ceiling ceiling;
     private Bed bed;
     private Lamp lamp;
     private Wardrobe wardrobe;
-    private Carpet carpet;
 
 
-    public Bedroom(String name, Wall wall1, Wall wall3, Wall wall4, Wall wall2, Ceiling ceiling, Bed bed, Lamp lamp, Wardrobe wardrobe, Carpet carpet) {
-        this.name = name;
-        this.wall1 = wall1;
-        this.wall3 = wall3;
-        this.wall4 = wall4;
-        this.wall2 = wall2;
-        this.ceiling = ceiling;
+
+    public Bedroom(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4,
+                   Carpet carpet, Ceiling ceiling,
+                   Bed bed, Lamp lamp, Wardrobe wardrobe) {
+
+        super(name, wall1, wall2, wall3, wall4, carpet, ceiling);
         this.bed = bed;
         this.lamp = lamp;
         this.wardrobe = wardrobe;
-        this.carpet = carpet;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Wall getWall1() {
-        return wall1;
-    }
-
-    public Wall getWall2() {
-        return wall2;
-    }
-
-    public Wall getWall3() {
-        return wall3;
-    }
-
-    public Wall getWall4() {
-        return wall4;
-    }
-
-    public Ceiling getCeiling() {
-        return ceiling;
     }
 
     public Bed getBed() {
@@ -63,9 +30,7 @@ public class Bedroom {
         return wardrobe;
     }
 
-    public Carpet getCarpet() {
-        return carpet;
-    }
+
 
 
 }
---
+++ src/main/java/org/example/model/Room.java
@@ -0,0 +1,36 @@
+package org.example.model;
+
+public class Room {
+    private String name;
+    private Wall wall1;
+    private Wall wall2;
+    private Wall wall3;
+    private Wall wall4;
+    private Ceiling ceiling;
+    private Carpet carpet;
+
+
+    public Room(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Carpet carpet, Ceiling ceiling) {
+        this.name = name;
+        this.wall1 = wall1;
+        this.wall2 = wall2;
+        this.wall3 = wall3;
+        this.wall4 = wall4;
+        this.carpet = carpet;
+        this.ceiling = ceiling;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Carpet getCarpet() {
+        return carpet;
+    }
+
+    public Ceiling getCeiling() {
+        return ceiling;
+    }
+
+
+}
